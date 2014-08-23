24264e1b8c67b12c3a7f905b6a9cc4b218b5056d
Moe <moe@diccuric.org>
2014-08-23 13:56:59 +0200

    Turning gun; add generator blocks

--- vehicle.lua
+++ vehicle.lua
@@ -3,10 +3,25 @@ class "Vehicle" {
   y = 0;
 }
 
-function Vehicle:__init()
-	
+function Vehicle:__init(x, y)
+	self.x = x
+  self.y = y
+  
+  self.vehicle = love.graphics.newImage('gfx/player.png')
+  self.tower = love.graphics.newImage('gfx/tower.png')
+  
+  self.offsetGunX = (self.vehicle:getWidth() - self.tower:getWidth()) / 2
+  self.offsetGunY = (self.vehicle:getHeight() - self.tower:getHeight()) / 2
 end
 
 function Vehicle:update(dt)
 
+end
+
+function Vehicle:draw()
+  love.graphics.draw(self.vehicle, self.x, self.y)
+  
+  local x, y = love.mouse.getPosition()
+  local r = math.atan2(x -self.x, self.y - y)
+  love.graphics.draw(self.tower, self.x + self.offsetGunX, self.y + self.offsetGunY, r, 1, 1, self.tower:getWidth() / 2, self.tower:getHeight() / 2)
 end
--- world.lua
+++ world.lua
@@ -6,15 +6,36 @@ class "World" {
 function World:__init(width, height)
 	self.width = width
   self.height = height
+  
+  self:generateUndestroyableObjects(10)
+  self:generateDestroyableObjects(10)
+  self:generateEnemies(20)
+  
+  self.player = Vehicle:new(20, 20)
+  
   self.backgroundImg = love.graphics.newImage('gfx/background.png')
   self.backgroundImg:setWrap('repeat', 'repeat')
   self.backgroundQuad = love.graphics.newQuad(0, 0, width, height, self.backgroundImg:getWidth(), self.backgroundImg:getHeight())
 end
 
-function World:update(dt)
+function World:generateUndestroyableObjects(count)
 
+end
+
+function World:generateDestroyableObjects()
+  
+end
+
+function World:generateEnemies()
+  
+end
+
+function World:update(dt)
+  self.player:update(dt)
 end
 
 function World:draw()
   love.graphics.draw(self.backgroundImg, self.backgroundQuad, 0, 0)
+  
+  self.player:draw()
 end
